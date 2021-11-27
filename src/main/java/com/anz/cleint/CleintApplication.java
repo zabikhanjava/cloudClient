45b5747637feb782682ffd9948a839c9152c7f46
zabikhan <zabikhan6565@gmail.com>
2021-11-27 22:28:45 +0530

    adding client repo

---
+++ src/main/java/com/anz/cleint/CleintApplication.java
@@ -0,0 +1,13 @@
+package com.anz.cleint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleintApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anz/cleint/CleintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anz.cleint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
